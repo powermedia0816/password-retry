e2b944335e6d6d600adf522c94fc2b6c8d08512d
powermedia0816 <jessicayang1004@gmail.com>
2024-04-06 11:43:14 +0800

    change to while > 0 version

--- password-retry.py
+++ password-retry.py
@@ -1,6 +1,6 @@
 password = 'a123456'
 i = 3
-while True:
+while i > 0:
 	pwd = input('請輸入密碼:')
 	if pwd == password:
 		print('恭喜你成功登入')
@@ -8,5 +8,3 @@ while True:
 	else:
 		i = i - 1
 		print('密碼錯誤，剩下',i,'幾次機會')
-		if i == 0:
-			break

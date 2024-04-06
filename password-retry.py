b76e5bf094b2372091778e06b189916200874a3f
powermedia0816 <jessicayang1004@gmail.com>
2024-04-06 11:54:43 +0800

    adding if else at last part

--- password-retry.py
+++ password-retry.py
@@ -1,10 +1,14 @@
 password = 'a123456'
 i = 3
 while i > 0:
+	i = i - 1
 	pwd = input('請輸入密碼:')
 	if pwd == password:
 		print('恭喜你成功登入')
 		break
 	else:
-		i = i - 1
-		print('密碼錯誤，剩下',i,'幾次機會')
+		print('密碼錯誤')
+		if i > 0 :
+			print('還有幾',i,'次機會')
+		else:
+			print('已經三次錯誤了，帳號被鎖住了')
